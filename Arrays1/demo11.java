047f7946f3e2282f4e0296e51ca4ca8ee3252c9d
Sahil <sahil.brahmbhatt710@gmail.com>
2025-12-26 19:28:30 +0530

    Added the Arrays1 folder

---
+++ Arrays1/demo11.java
@@ -0,0 +1,42 @@
+// Create a program to find the transpose of a 2-D array (matrix).
+import java.util.Scanner;
+
+public class demo11 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = input.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int cols = input.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+        int[][] transpose = new int[cols][rows];
+
+        // input matrix
+        System.out.println("Enter elements of matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = input.nextInt();
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+       
+        System.out.println("Transpose of matrix:");
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                System.out.print(transpose[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
